441238ccca7b9d3ec01ef855aac021ec9e1d1a12
Jobayer Hossain <54477945+jobayer637@users.noreply.github.com>
2020-12-12 04:45:41 +0600

    update and add cart system

--- src/shop/index.jsx
+++ src/shop/index.jsx
@@ -5,6 +5,7 @@ import Body from './body/index'
 import Footer from './footer/index'
 import axios from 'axios';
 import AllRouter from './router/router'
+import CartModal from './cart/index'
 
 class Shop extends Component {
 
@@ -12,7 +13,10 @@ class Shop extends Component {
         products: [],
         filter: '',
         categoryFilter: '',
-        categories: []
+        categories: [],
+        quantity: 1,
+        cart: [],
+        cartModalIsOpen: false
     }
 
     componentDidMount = () => {
@@ -41,6 +45,52 @@ class Shop extends Component {
         })
     }
 
+    handleChangeAddToCard = event => {
+        this.setState({ quantity: event.target.value })
+    }
+
+    handleProductAddToCard = id => {
+        const product = this.state.products.find(product => product.id === id)
+        const prevCart = [...this.state.cart]
+        const findPrev = prevCart.findIndex(cart => cart.productId === product.id)
+        const storePrev = prevCart[findPrev]
+        if (storePrev) {
+            storePrev.totalPrice += (product.price * this.state.quantity)
+            storePrev.quantity = parseInt(storePrev.quantity) + parseInt(this.state.quantity)
+        }
+
+
+        const cart = {
+            userId: 1,
+            productId: product.id,
+            productTitle: product.title,
+            quantity: this.state.quantity,
+            unitPrice: product.price,
+            totalPrice: this.state.quantity * product.price,
+            image: product.image
+        }
+
+        if (findPrev === -1) {
+            this.setState({
+                cart: this.state.cart.concat(cart),
+                quantity: 1
+            }, () => {
+                //console.log(this.state.cart)
+            })
+        } else {
+            this.setState({
+                cart: prevCart,
+                quantity: 1
+            })
+            // console.log(storePrev)
+        }
+
+    }
+
+    handleCartModal = () => {
+        this.setState({ cartModalIsOpen: !this.state.cartModalIsOpen})
+    }
+
     render() {
         return <div className="container my-3">
             <div className="px-5">
@@ -48,6 +98,14 @@ class Shop extends Component {
                 <Header
                     filter={this.state.filter}
                     handleSearch={this.handleSearch}
+                    handleCartModal={this.handleCartModal}
+                    totalCarts={this.state.cart.length}
+                />
+
+                <CartModal
+                    isOpen={this.state.cartModalIsOpen}
+                    handleCartModal={this.handleCartModal}
+                    products={this.state.cart}
                 />
 
                 <AllRouter
@@ -56,8 +114,11 @@ class Shop extends Component {
                     categoryFilter={this.state.categoryFilter}
                     categories={this.state.categories}
                     handleCategoryFilter={this.handleCategoryFilter}
+                    handleChangeAddToCard={this.handleChangeAddToCard}
+                    handleProductAddToCard={this.handleProductAddToCard}
+                    quantity={this.state.quantity}
                 />
-                
+
                 <Footer />
             </div>
         </div>
